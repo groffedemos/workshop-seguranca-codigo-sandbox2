0746a395cd165ffd0a20c15489c76b2b945573b7
Renato Groffe <renato.groffe@yahoo.com.br>
2025-08-28 15:26:09 -0300

    Commit inicial

---
+++ src/ConsoleAppJobNotificacaoHttp/Models/InfoExecucao.cs
@@ -0,0 +1,9 @@
+namespace ConsoleAppJobNotificacaoHttp.Models;
+
+public class InfoExecucao
+{
+    public string? Instancia { get; set; }
+    public string? Kernel { get; set; }
+    public string? Framework { get; set; }
+    public string? Horario { get; set; }
+}
